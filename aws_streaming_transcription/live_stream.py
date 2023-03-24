6fb95067adcaf2eae4a390a8dace166e2fb7a8a3
Erisae <xiayuhan18@mails.ucas.ac.cn>
2023-03-24 12:31:57 -0400

    add aws stream transcription / recoding / openai translation

---
+++ aws_streaming_transcription/live_stream.py
@@ -0,0 +1,64 @@
+import asyncio
+import sounddevice as sd
+import numpy as np
+
+from amazon_transcribe.client import TranscribeStreamingClient
+from amazon_transcribe.handlers import TranscriptResultStreamHandler
+from amazon_transcribe.model import TranscriptEvent
+
+'''
+https://python-sounddevice.readthedocs.io/en/0.3.7/
+sd.decault.device = #
+'''
+
+audio_min_rms = 40
+max_low_audio_flag = 20
+
+class MyEventHandler(TranscriptResultStreamHandler):
+    async def handle_transcript_event(self, transcript_event: TranscriptEvent):
+        results = transcript_event.transcript.results
+        for result in results:
+            for alt in result.alternatives:
+                print(alt.transcript)
+
+
+async def basic_transcribe():
+    client = TranscribeStreamingClient(region="us-east-2")
+
+    stream = await client.start_stream_transcription(
+        language_code="en-US",
+        media_sample_rate_hz=16000,
+        media_encoding="pcm",
+    )
+
+    async def write_chunks():
+        low_audio_flag = 0
+        while True:
+           
+            data = sd.rec(1024*8, samplerate=16000, channels=1, blocking=True, dtype='int16')
+
+            data[np.isnan(data)]=0
+            rms  = np.sqrt(np.mean(np.square(data)))
+
+            # print(low_audio_flag)
+
+            low_audio_flag = 0 if rms > audio_min_rms else low_audio_flag + 1
+
+            # 100 consecutive samples of low audio
+            if low_audio_flag > max_low_audio_flag:
+                break
+
+            # convert the audio frame to a byte stream and send it to the transcription service.
+            await stream.input_stream.send_audio_event(audio_chunk=data.tobytes())
+        
+        await stream.input_stream.end_stream()
+            
+
+
+    handler = MyEventHandler(stream.output_stream)
+    await asyncio.gather(write_chunks(), handler.handle_events())
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(basic_transcribe())
+loop.close()
---
+++ aws_streaming_transcription/prerecorded_stream.py
@@ -0,0 +1,76 @@
+# reference: https://github.com/awslabs/amazon-transcribe-streaming-sdk
+# document:  https://docs.aws.amazon.com/transcribe/latest/dg/streaming.html
+'''
+default installation requires aws-crt~=0.14 but 0.14 can not be installed 
+    - install manually by cloning the sdk and change setup.py and setup.cfg 's awscrt requiremente to >= 0.14
+    - 0.16 works
+'''
+import asyncio
+
+# This example uses aiofile for asynchronous file reads.
+# It's not a dependency of the project but can be installed
+# with `pip install aiofile`.
+import aiofile
+
+from amazon_transcribe.client import TranscribeStreamingClient
+from amazon_transcribe.handlers import TranscriptResultStreamHandler
+from amazon_transcribe.model import TranscriptEvent
+from amazon_transcribe.utils import apply_realtime_delay
+
+"""
+Here's an example of a custom event handler you can extend to
+process the returned transcription results as needed. This
+handler will simply print the text out to your interpreter.
+"""
+
+
+SAMPLE_RATE = 16000
+BYTES_PER_SAMPLE = 2
+CHANNEL_NUMS = 1
+
+# An example file can be found at tests/integration/assets/test.wav
+AUDIO_PATH = "../audio/test.wav"
+CHUNK_SIZE = 1024 * 8
+REGION = "us-west-2"
+
+
+class MyEventHandler(TranscriptResultStreamHandler):
+    async def handle_transcript_event(self, transcript_event: TranscriptEvent):
+        # This handler can be implemented to handle transcriptions as needed.
+        # Here's an example to get started.
+        results = transcript_event.transcript.results
+        for result in results:
+            for alt in result.alternatives:
+                print(alt.transcript)
+
+
+async def basic_transcribe():
+    # Setup up our client with our chosen AWS region
+    client = TranscribeStreamingClient(region=REGION)
+
+    # Start transcription to generate our async stream
+    stream = await client.start_stream_transcription(
+        language_code="en-US",
+        media_sample_rate_hz=SAMPLE_RATE,
+        media_encoding="pcm",
+    )
+
+    async def write_chunks():
+        # NOTE: For pre-recorded files longer than 5 minutes, the sent audio
+        # chunks should be rate limited to match the realtime bitrate of the
+        # audio stream to avoid signing issues.
+        async with aiofile.AIOFile(AUDIO_PATH, "rb") as afp:
+            reader = aiofile.Reader(afp, chunk_size=CHUNK_SIZE)
+            await apply_realtime_delay(
+                stream, reader, BYTES_PER_SAMPLE, SAMPLE_RATE, CHANNEL_NUMS
+            )
+        await stream.input_stream.end_stream()
+
+    # Instantiate our handler and start processing events
+    handler = MyEventHandler(stream.output_stream)
+    await asyncio.gather(write_chunks(), handler.handle_events())
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(basic_transcribe())
+loop.close()
